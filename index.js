d02a7b981d956b993474c9ded14a5b55185afe65
GALBY <william.galby@cgi.com>
2017-02-24 17:03:56 +0100

    2ème étape

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 let chalk = require('chalk');
 
-console.log(chalk.green('Hello world'));
+// console.log(chalk.green('Hello world'));
+
+//ES5
+function myEvery(values, acc) {
+    let result = true;
+    if(acc !== undefined) {
+        result = acc;
+    }
+    console.log('RESULT', result);
+    console.log('ARRAY', values);
+    if(values && values.length > 0) {
+        console.log('coucou');
+        console.log('ELEMENT', values[0]);
+        return myEvery(values.slice(1), values[0]);
+    } else {
+        return true;
+    }
+}
+
+// ES2015
+// let myEvery = () => {
+//     return true;
+// };
+
+module.exports = {
+    myEvery: myEvery
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,27 @@
 var expect = require('chai').expect;
 
+var myEvery = require('../index').myEvery;
 
-describe('Hello World', function() {
+describe('function myEvery in recursive', function() {
 
-  it('True should be true', function() {
-    expect(true).to.be.true;
+  it('No array should return true', function() {
+    expect(myEvery()).to.be.true;
+  });
+
+  it('Empty array should return true', function() {
+    expect(myEvery([])).to.be.true;
+  });
+
+  it('Array with 1 element false should return false', function() {
+    expect(myEvery([false])).to.be.false;
+  });
+
+  it('Array with 1 element true should return true', function() {
+    expect(myEvery([true])).to.be.true;
+  });
+
+  it('Array with 2 elements true & false should return false', function() {
+    expect(myEvery([true, false])).to.be.false;
   });
 
 });
